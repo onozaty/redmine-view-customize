48407aca867b733de2849b141292c1ca779a5eb9
onozaty <onozaty@gmail.com>
2018-07-22 22:59:00 +0900

    Add insertion position
    It made it possible to select where to insert the code.
    * All html head
    * Issue form bottom
    * Issue show bottom

--- lib/view_customize/view_layouts_base_html_head_hook.rb
+++ lib/view_customize/view_layouts_base_html_head_hook.rb
@@ -2,22 +2,46 @@ module RedmineViewCustomize
   class ViewLayoutsBaseHtmlHeadHook < Redmine::Hook::ViewListener
     def view_layouts_base_html_head(context={})
 
-      header = "\n<!-- [view customize plugin] path:#{context[:request].path_info} -->\n"
+      path = context[:request].path_info;
+
+      header = "\n<!-- [view customize plugin] path:#{path} -->\n"
       header << stylesheet_link_tag("view_customize", plugin: "view_customize")
 
-      view_customize_html_parts = match_customize(context[:request].path_info).map {|item|
+      view_customize_html_parts = match_customize(path, ViewCustomize::INSERTION_POSITION_HTML_HEAD).map {|item|
         html(item)
       }
       header << view_customize_html_parts.join("\n").html_safe
       return header
     end
 
+    def view_issues_form_details_bottom(context={})
+
+      path = context[:request].path_info;
+
+      view_customize_html_parts = match_customize(path, ViewCustomize::INSERTION_POSITION_ISSUE_FORM).map {|item|
+        html(item)
+      }
+      return view_customize_html_parts.join("\n").html_safe
+    end
+
+    def view_issues_show_details_bottom(context={})
+
+      path = context[:request].path_info;
+
+      view_customize_html_parts = match_customize(path, ViewCustomize::INSERTION_POSITION_ISSUE_SHOW).map {|item|
+        html(item)
+      }
+      return view_customize_html_parts.join("\n").html_safe
+    end
+
     private
 
-    def match_customize(path)
+    def match_customize(path, insertion_position)
 
       ViewCustomize.all.select {|item|
-        item.available? && path =~ Regexp.new(item.path_pattern)
+        item.available? \
+          && item.insertion_position == insertion_position \
+          && path =~ Regexp.new(item.path_pattern)
       }
     end
 
