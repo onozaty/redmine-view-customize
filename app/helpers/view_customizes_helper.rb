003e2e2159daeb2a31bb8dff4c040b9c722652aa
onozaty <onozaty@gmail.com>
2018-10-08 01:09:17 +0900

    [Redime4.0] Change to not call CodeRay directly by syntax highlight

--- app/helpers/view_customizes_helper.rb
+++ app/helpers/view_customizes_helper.rb
@@ -1,7 +1,7 @@
 module ViewCustomizesHelper
   def highlight_by_language(code, language)
     ("<pre><code class=\"#{language} syntaxhl\">" +
-         CodeRay.scan(code, language).html(:wrap => :span) +
+         Redmine::SyntaxHighlighting.highlight_by_language(code, language) +
          "</code></pre>").html_safe
   end
 end
