d2e17b842dec52afa9dbdde53ca946546cd95348
onozaty <onozaty@gmail.com>
2018-10-21 01:31:24 +0900

    Fix extra line break are displayed when syntax highlight

--- app/helpers/view_customizes_helper.rb
+++ app/helpers/view_customizes_helper.rb
@@ -1,5 +1,6 @@
 module ViewCustomizesHelper
   def highlight_by_language(code, language)
+    code.gsub!(/(\r\n|\r|\n)/, "\n")
     ("<pre><code class=\"#{language} syntaxhl\">" +
          Redmine::SyntaxHighlighting.highlight_by_language(code, language) +
          "</code></pre>").html_safe
