134ffc8fb10041b90499d8f9c880497cd44208d1
onozaty <onozaty@gmail.com>
2019-10-06 13:09:39 +0900

    Change ViewCustomize.context.user.last_login_on to lastLoginOn

--- lib/view_customize/view_hook.rb
+++ lib/view_customize/view_hook.rb
@@ -90,7 +90,7 @@ module RedmineViewCustomize
           "admin" => user.admin?,
           "firstname" => user.firstname,
           "lastname" => user.lastname,
-          "last_login_on" => (user.last_login_on.iso8601 unless user.last_login_on.nil?),
+          "lastLoginOn" => (user.last_login_on.iso8601 unless user.last_login_on.nil?),
           "groups" => user.groups.map {|group| { "id" => group.id, "name" => group.name }},
           "apiKey" => (user.api_token.value unless user.api_token.nil?),
           "customFields" => user.custom_field_values.map {|field| { "id" => field.custom_field.id, "name" => field.custom_field.name, "value" => field.value }}
