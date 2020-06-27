4c090e8cd654269d2c263adb022d55a425a98510
onozaty <onozaty@gmail.com>
2020-06-28 00:16:43 +0900

    Change change_column_null syntax

--- db/migrate/007_change_path_pattern_default_on_view_customizes.rb
+++ db/migrate/007_change_path_pattern_default_on_view_customizes.rb
@@ -2,6 +2,6 @@ class ChangePathPatternDefaultOnViewCustomizes < ActiveRecord::CompatibleLegacyM
   def change
     change_column_default :view_customizes, :path_pattern, from: nil, to: ""
     change_column_default :view_customizes, :comments, from: nil, to: ""
-    change_column_null :view_customizes, :comments, null: false, ""
+    change_column_null :view_customizes, :comments, false, ""
   end
 end
