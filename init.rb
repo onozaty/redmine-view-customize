1ee381d52a6bddebf19e2595e34d8e92bf770d00
onozaty <onozaty@gmail.com>
2023-01-16 22:13:13 +0900

    Update version number to 3.3.0

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :view_customize do
   name 'View Customize plugin'
   author 'onozaty'
   description 'View Customize plugin for Redmine'
-  version '3.2.0'
+  version '3.3.0'
   url 'https://github.com/onozaty/redmine-view-customize'
   author_url 'https://github.com/onozaty'
 
