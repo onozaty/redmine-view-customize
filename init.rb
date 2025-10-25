dfee75df6e063ab3e08b0dfbb3613d7bc4c03c18
onozaty <onozaty@gmail.com>
2025-10-25 04:49:55 +0000

    Bump version to 3.5.3

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :view_customize do
   name 'View Customize plugin'
   author 'onozaty'
   description 'View Customize plugin for Redmine'
-  version '3.5.2'
+  version '3.5.3'
   url 'https://github.com/onozaty/redmine-view-customize'
   author_url 'https://github.com/onozaty'
 
